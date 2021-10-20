a4040a3af1f1966d75becf25ceb6475a629c28d0
unknown <you@example.com>
2021-10-20 16:19:22 +0300

    home banner added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <Header />
-       <h3>Hello, Code Cleaned!</h3>
-    </div>
+      <Home />
+     </div>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './home.css'
+
+function Home(){
+    return(
+
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image"
+                src="https://m.media-amazon.com/images/I/61ASx7NHTWL._SX3000_.jpg" alt="" />
+              <h1>hiiiii</h1>
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
