20e3ac1d041f96814208d7f8692e6a13d0db0abc
unknown <you@example.com>
2021-10-20 15:07:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+       <h3>Hello, Code Cleaned!</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBtJYg9k701CSY3MVOpB4CNeWyGB7L35tg",
+    authDomain: "clone-98041.firebaseapp.com",
+    projectId: "clone-98041",
+    storageBucket: "clone-98041.appspot.com",
+    messagingSenderId: "166647548150",
+    appId: "1:166647548150:web:5cf034d6407139bec604d7",
+    measurementId: "G-NVM00VPCJP"
+  };
